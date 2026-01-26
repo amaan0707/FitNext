caff1c764060126026a6ae0976b29a00a2442657
Amaan Ali <136046113+amaan0707@users.noreply.github.com>
2026-01-26 17:46:08 +0530

    jan 26 commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,11 @@ plugins {
 
 android {
     namespace = "com.example.fitnext"
-    compileSdk = 34
-
+    compileSdk = 36
     defaultConfig {
         applicationId = "com.example.fitnext"
         minSdk = 31
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -30,33 +29,33 @@ android {
 
 dependencies {
 
-    implementation("com.google.android.gms:play-services-auth:21.2.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-auth:23.1.0")
-    implementation("com.google.firebase:firebase-messaging:24.0.3")
-    implementation("androidx.media3:media3-common:1.4.1")
+    implementation("com.google.android.gms:play-services-auth:21.5.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("com.google.firebase:firebase-auth:24.0.1")
+    implementation("com.google.firebase:firebase-messaging:25.0.1")
+    implementation("androidx.media3:media3-common:1.9.1")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation ("com.firebaseui:firebase-ui-database:8.0.2")
-    implementation ("com.firebaseui:firebase-ui-firestore:8.0.2")
-    implementation ("com.firebaseui:firebase-ui-auth:8.0.2")
-    implementation ("com.firebaseui:firebase-ui-storage:8.0.2")
-    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
+    implementation("androidx.core:core-splashscreen:1.2.0")
+    implementation ("com.firebaseui:firebase-ui-database:9.1.1")
+    implementation ("com.firebaseui:firebase-ui-firestore:9.1.1")
+    implementation ("com.firebaseui:firebase-ui-auth:9.1.1")
+    implementation ("com.firebaseui:firebase-ui-storage:9.1.1")
+    implementation(platform("com.google.firebase:firebase-bom:34.8.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-firestore")
-    implementation ("com.google.firebase:firebase-database:21.0.0")
-    implementation ("com.hbb20:ccp:2.7.1")
+    implementation ("com.google.firebase:firebase-database:22.0.1")
+    implementation ("com.hbb20:ccp:2.7.3")
 
     implementation ("io.github.chaosleung:pinview:1.4.4")
-    implementation ("pl.droidsonroids.gif:android-gif-drawable:1.2.28")
+    implementation ("pl.droidsonroids.gif:android-gif-drawable:1.2.30")
 
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation("androidx.activity:activity-ktx:1.9.3") // Or latest version
-    implementation("androidx.fragment:fragment-ktx:1.8.4")
+    implementation("com.squareup.okhttp3:okhttp:5.3.2")
+    implementation("androidx.activity:activity-ktx:1.12.2") // Or latest version
+    implementation("androidx.fragment:fragment-ktx:1.8.9")
     implementation("com.google.firebase:firebase-inappmessaging-display")
     implementation("com.google.firebase:firebase-analytics")
 
--- app/src/main/java/com/example/fitnext/LoginActivity.java
+++ app/src/main/java/com/example/fitnext/LoginActivity.java
@@ -1,3 +1,4 @@
+// Hello World
 package com.example.fitnext;
 
 import androidx.annotation.NonNull;
